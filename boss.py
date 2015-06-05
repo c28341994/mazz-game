1e1904f45ee87b70f2b146f0b66b19a65bca9395
e24016611 <sunboyboy1@gmail.com>
2015-06-06 00:44:37 +0800

    add boss gamehelper

---
+++ boss.py
@@ -0,0 +1,23 @@
+from player import Player
+
+class boss(Player):
+
+
+    def __init__(self):
+
+        self.__hp = 50
+        self.__dmg = 0
+    
+    
+    def test(self):
+        pass
+
+
+
+
+
+if __name__ == "__main__" :
+
+    b = boss()
+    x = b.atk()
+    print x
